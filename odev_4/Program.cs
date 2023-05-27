e03d03489a2f18c85d225c07aa8734dd752e2802
Hp <Hp@DESKTOP-LTJOJD3>
2023-05-27 21:39:10 +0300

    Add project files.

---
+++ odev_4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odev_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int c_toplam = 0;
+            int t_toplam = 0;
+
+            int a = 0;
+            while (a <= 20)
+            {
+                if (a % 2 == 0)
+                {
+                    c_toplam += a;
+                }
+                else
+                {
+                    t_toplam = t_toplam + a;
+                }
+                a++;
+            }
+            Console.WriteLine("Çift Sayıların Toplamı : " + c_toplam);
+            Console.WriteLine("Tek Sayıların Toplamı : " + t_toplam);
+            Console.ReadKey();
+        }
+    }
+}
